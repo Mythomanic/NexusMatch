f6b760b5f23c9887bf4965e122eaef9e95a278b4
Necati Doğrul <necatidogrul7@gmail.com>
2024-05-17 01:21:36 +0300

    swipe action frontend

--- Frontend/nexus-match/src/TinderCards.js
+++ Frontend/nexus-match/src/TinderCards.js
@@ -1,17 +1,40 @@
-import React from 'react';
-
-
-
-
-
-
+import React, { useState } from 'react';
+import TinderCard from 'react-tinder-card'
+import './TinderCards.css';
 
 function TinderCards() {
+  const [people, setPeople] = useState([
+    {
+        name: 'Elon Musk',
+        url: 'https://picsum.photos/200'
+    },
+    {
+        name: 'Jeff Bezos',
+        url: 'https://picsum.photos/201'
+    }
+  ]);
+
   return (
     <div>
         <h1>Match Cards</h1>
+
+        <div className='tinderCards__cardContainer'>
+            {people.map((person) => (
+            <TinderCard 
+            className='swipe'
+            key={person.name} 
+            preventSwipe={['up', 'down']}>
+                <div 
+                style={{backgroundImage: `url(${person.url})`}}
+                className='card'>
+                    <h3>{person.name}</h3>
+                </div>
+            </TinderCard>
+        ))}
+        </div>
+        
     </div>
-  )
+  );
 }
 
-export default TinderCards
+export default TinderCards;
