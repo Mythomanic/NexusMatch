2792e1e851497da99f1a3beb54444df25a3cbd4d
Ahmet Can DERİCİOĞLU <4hcan2001@gmail.com>
2024-05-19 17:15:14 +0300

    chat api added

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,15 +1,28 @@
 <?php
 
-use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\ChatController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
 Route::post('/auth/register', [UserController::class, 'create'])->name('register');
 Route::post('/auth/login', [UserController::class, 'login'])->name('login');
+
+Route::post('messages', [ChatController::class, 'message']);
 
 Route::get('/user/{id}', [UserController::class,'show'])->name('get.user');
 Route::get('/user', [UserController::class,'showAll'])->name('getAll.user');
 Route::post('/user/edit/{id}', [UserController::class,'edit'])->name('edit.user');
 Route::delete('/user/delete/{id}', [UserController::class,'destroy'])->name('delete.user');
-
--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\PusherController@index');
+Route::post('/broadcast', 'App\Http\Controllers\PusherController@broadcast');
+Route::post('/receive', 'App\Http\Controllers\PusherController@receive');
