2865a1cd368e9fa3b2c009b55d3f4496fde20eb1
Ahmet Can DERİCİOĞLU <4hcan2001@gmail.com>
2024-06-10 12:39:51 +0300

    pusher config changed

--- Backend/routes/channels.php
+++ Backend/routes/channels.php
@@ -13,7 +13,7 @@ use App\Models\Chat;
 | used to check if an authenticated user can listen to the channel.
 |
 */
-Broadcast::channel('chat.{chatId}', function ($user, $chatId) {
+Broadcast::channel('private-chat.{chatId}', function ($user, $chatId) {
     $chat = Chat::find($chatId);
 
     return $chat && $chat->users->contains($user->id);
